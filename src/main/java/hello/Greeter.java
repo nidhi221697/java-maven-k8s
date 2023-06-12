ed548f447237275365cd5db88dcf68295cd8c86c
Nidhi <113484706+nidhi221697@users.noreply.github.com>
2023-06-12 16:49:57 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project for AchiStar Technologies";
+        return "DevOps Project to deploy a container on ec2 having k8s container management solutions by Nidhi Singh";
     }
 }
